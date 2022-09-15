765cb382eeb0e4880ca20a277f8423b49f792c89
Matheus Schreiner Homrich Da Silva <Matheus.Silva00@facin.portoalegre.pucrsnet.br>
2022-09-15 10:19:09 -0300

    todo

---
+++ unhash.c
@@ -0,0 +1,22 @@
+int main(int argc, char *argv[]){
+	
+    // cria cojunto teste
+    // alfabeto
+    char letters [26] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    char capitalLetters [26] = toupper(letters)
+    char alph[] = {'.',',',':','?','=','-','(',')',,'/','%','@','!'};
+    char numbers [9] = {0,1,2,3,4,5,6,7,8,9}
+    // flag dificudade
+    uint8_t *result;
+    
+    //geradorDeSenha
+
+	result = md5String(argv[i]);
+	print_hash(result);
+	
+    // for md5 
+        // if result md5String(geradorDeSenha);
+
+
+	free(result);
+}
