6f66132a15607d69a4f8e0a2b71ad340072919bf
Matheus Homrich <matidasilva34@gmail.com>
2022-09-24 16:40:49 -0300

    unhash updated

--- unhash.c
+++ unhash.c
@@ -6,68 +6,80 @@
 #include "md5.h"
 #include "printer.h"
 #include "time.h"
+#include <omp.h>
 
 #define EASY 0
 #define MEDIUM 1
 #define HARD 2
 
-int generateRandomNum() {
-    return rand()%26;
+int generateRandomNum(int seed)
+{
+    return rand() % 26;
 }
 
-void generatePassWord(int size, int difficulty, char letters[], char result[]){
-    if (difficulty == EASY) {
+void generatePassWord(int size, int difficulty, char letters[], char result[], int seed)
+{
+    if (difficulty == EASY)
+    {
         for (size_t i = 0; i < size; i++)
         {
-            int randomNumber = generateRandomNum();
+            int randomNumber = generateRandomNum(seed);
             // printf("Numero gerado:%d letra -> %c ",randomNumber, letters[randomNumber]);
             result[i] = letters[randomNumber];
-            if(i == size - 1){
-                result[i+1] = 0;
+            if (i == size - 1)
+            {
+                result[i + 1] = 0;
             }
         }
-    } else if (difficulty == MEDIUM) {
+    }
+    else if (difficulty == MEDIUM)
+    {
         // printf("MEDIUM");
-    } else if (difficulty == HARD) {
+    }
+    else if (difficulty == HARD)
+    {
         // printf("HARD");
     }
 }
 
-
-int main(int argc, char *argv[]){
-    char letters [26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    //char capitalLetters [26] = toupper(letters);
-    char alph[] = {'.',',',':','?','=','-','(',')','/','%','@','!'};
-    char numbers [10] = {0,1,2,3,4,5,6,7,8,9};
+int main(int argc, char *argv[])
+{
+    char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    // char capitalLetters [26] = toupper(letters);
+    char alph[] = {'.', ',', ':', '?', '=', '-', '(', ')', '/', '%', '@', '!'};
+    char numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     uint8_t *hash;
     uint8_t *result;
     int dificuldade = atoi(argv[2]);
-    char* senha = argv[1];
+    char *senha = argv[1];
     int tamanhoSenha = strlen(senha);
-	result = md5String(senha);   
-
+    result = md5String(senha);
+    srand(atoi(argv[3]));
     int count = 0;
     clock_t t;
     t = clock();
 
     char generatedPassWord[tamanhoSenha + 1];
-    generatePassWord(tamanhoSenha, dificuldade, letters, generatedPassWord);
- 	hash = md5String(generatedPassWord);
-    while (!compare(hash, result)) {
-        //printf("Tamanho da senha: %d\n",tamanhoSenha);
-        generatePassWord(tamanhoSenha, dificuldade, letters, generatedPassWord);
-        //printf("Gerador de senha(Senha antes do hash): %s \n",generatedPassWord);
+    generatePassWord(tamanhoSenha, dificuldade, letters, generatedPassWord, atoi(argv[3]));
+    hash = md5String(generatedPassWord);
+
+    #pragma omp parallel num_threads(12)
+    while (!compare(hash, result))
+    {
+        // printf("Tamanho da senha: %d\n",tamanhoSenha);
+        generatePassWord(tamanhoSenha, dificuldade, letters, generatedPassWord, atoi(argv[3]));
+        // printf("Gerador de senha(Senha antes do hash): %s \n",generatedPassWord);
         hash = md5String(generatedPassWord);
-        //printf("Gerador de senha(Depois do hash): ");
-        //print_bytes(hash,16);  
-        //printf("Hash sendo procurado: ");
-        //print_bytes(result,16);  
-        //printf("\n");
-        count ++;
+        // printf("Gerador de senha(Depois do hash): ");
+        // print_bytes(hash,16);
+        // printf("Hash sendo procurado: ");
+        // print_bytes(result,16);
+        // printf("\n");
+        count++;
     }
 
     t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
 
     printf("Tempo de execucao: %f  \n", time_taken);
     printf("Senha encontrada: %s\n", generatedPassWord);
